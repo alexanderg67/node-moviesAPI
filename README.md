1a788e1a2134a4640db2268087afd19804d44220
Alexander <divox5@gmail.com>
2023-05-11 17:37:22 +0300

    modified

--- README.md
+++ README.md
@@ -1,19 +1,38 @@
-# nodejs  -moviesAPI
-API to Get films array by filter from  mongodb \
-HTTP Method: GET \
-Endpoint URL: http://host:port/api/v1/films \
-Get params: \
-name - string in film name to search in database \
-rating -  Int value. search films with exact rating value, greater than, equal or  less than.  \
- Supports operators: >,<, = \
- Example:rating=>5 \
-genre - string with film genre .Supports values from  database genre array: "триллер", "драма" , "мелодрама" , "музыка", "мюзикл", "криминал" ,"боевик" \
-Example: 
-http://localhost:5000/api/v1/films?genre=%D0%BA%D0%BE%D0%BC%D0%B5%D0%B4%D0%B8%D1%8F&rating=%3E8&name=%D0%BC%D1%83  \
-HTML page category.html in public folder as example. <br /> 
-Create file .env and specify: \
-MONGO_URI - mongodb string \
-PORT - http port for API <br /> 
-Example: <br />
-MONGO_URI=mongodb://localhost/films <br />
+# Project Title
+
+API to get films with filter. Requires MongoDB connection
+
+## Description
+
+HTTP Method: GET
+Endpoint URL: http://host:port/api/v1/films
+Get params:
+name - string in film name to search in database
+rating - Int value. search films with exact rating value, greater than, equal or less than.
+Supports operators: >,<, =
+Example:rating=>5
+genre - string with film genre .Supports values from database genre array: "триллер", "драма" , "мелодрама" , "музыка", "мюзикл", "криминал" ,"боевик"
+Example: http://localhost:5000/api/v1/films?genre=%D0%BA%D0%BE%D0%BC%D0%B5%D0%B4%D0%B8%D1%8F&rating=%3E8&name=%D0%BC%D1%83
+http://localhost:3000/api/v1/films?rating=>7&limit=2&page=2
+HTML page category.html (in public folder) makes query as example.
+## Getting Started
+
+### Dependencies
+
+* Describe any prerequisites, libraries, OS version, etc., needed before installing program.
+* ex. Windows 10
+
+### Installing
+* run: npm install
+* Create file .env and specify:
+MONGO_URI - mongodb string
+PORT - http port for API
+Example:
+MONGO_URI=mongodb://localhost/films
 PORT=5000
+
+### Executing program
+
+* Run: npm start
+
+ 
--- controllers/films.js
+++ controllers/films.js
@@ -3,21 +3,30 @@ const searchFilms =async (req,res) => {
     try { 
        
     let queryObject={}
-    let {name,rating,genre}=req.query;
+    let {name,rating,genre,year}=req.query;
     if(name) { 
      queryObject.nameRu=new RegExp(name, 'i')
      
     }
+     
+    if( year && isFinite(year)){
+    queryObject.year=Number(year)
+    }
     if(rating) { 
-        rating=rating.replace(/>/gi,'$gte-' )
+         
+        rating=rating.replace(/>=/gi,'$gte-' )
+        rating=rating.replace(/<=/gi,'$lte-' )
+        rating=rating.replace(/>/gi,'$gt-' )
         rating=rating.replace(/=/gi,'$eq-' )
         rating=rating.replace(/</gi,'$lt-' )
         
+         
         const [operator,value] =rating.split('-')
         if(operator &&value && isFinite(value)) {
+            
         queryObject.imdbRating={[operator]: Number(value ) }
         }
-        
+         
 
     }
     if(genre) { 
@@ -28,13 +37,49 @@ const searchFilms =async (req,res) => {
     const skip=(page-1)* limit;
     //console.log('skip and limit:', skip,limit)
     //console.log(queryObject)
-    let result= film.find( queryObject ,{ __v:0 })
-    result=result.skip(skip).limit(limit)
-    const films= await result;
+    let films= await film.find( queryObject ,{ __v:0 }).skip(skip).limit(limit)
     res.status(200).json(films)
 }catch(error)  {
     res.status(500).json({msg: error})
 }
 }
+// Для тестового наполнения БД 
+const fillDB =async (req,res) => {
+    try{
+    let r = await film.create( {nameRu: 'Невероятная жизнь Уолтера Митти',
+    nameEn: 'The Secret Life of Walter Mitty',
+    year: 2013,
+    imdbRating: 7.9 ,
+    description: 'Stop Dreaming. Start Living',
+    genre: ['комедия','мелодрама' , 'драма']
+},
+{nameRu: 'Всегда говори ДА',
+    nameEn: 'Yes Man',
+    year: 2008,
+    imdbRating: 6.6 ,
+    description: 'Одно слово может изменить твою жизнь',
+    genre: ['комедия' ]
+},
+{nameRu: 'Эффект бабочки',
+    nameEn: 'Butterfly Effect',
+    year: 2003,
+    imdbRating: 8.2 ,
+    description: 'Изменишь одно, изменится все',
+    genre: ['фантастика', 'драма' ]
+},
+{nameRu: 'Исходный код',
+    nameEn: 'source code',
+    year: 2011,
+    imdbRating: 7.2 ,
+    description: 'Make every second count',
+    genre: ['фантастика', 'боевик' ]
+},
 
-module.exports= { searchFilms, }
+)
+ res.status(200).json({msg: 'films created'})
+}catch(error)  {
+    res.status(500).json({msg: error})
+}
+}
+
+module.exports= { searchFilms,fillDB }
